bae589b9990af5b8fd6538a7d39a198f1149084f
duyh80456-code <duyh80456@gmail.com>
2025-10-01 21:06:06 +0700

    Update TetrisGrid.java

--- oop-main/HW2/src/main/java/TetrisGrid.java
+++ oop-main/HW2/src/main/java/TetrisGrid.java
@@ -3,13 +3,14 @@
 // a clearRows() capability.
 
 public class TetrisGrid {
-	
+	private boolean[][] grid;
 	/**
 	 * Constructs a new instance with the given grid.
 	 * Does not make a copy.
 	 * @param grid
 	 */
 	public TetrisGrid(boolean[][] grid) {
+		this.grid = grid;
 	}
 	
 	
@@ -17,6 +18,28 @@ public class TetrisGrid {
 	 * Does row-clearing on the grid (see handout).
 	 */
 	public void clearRows() {
+		int counttrue =0;
+		boolean[][] newgrid =new boolean[grid.length][grid[0].length];
+		int n = 0;
+		for(int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (!grid[i][j]) {
+					continue;
+				}
+				for(int l = 0; l < newgrid[0].length; l++) {
+					newgrid[n][l] = false;
+				}
+				n++;
+
+			}
+		}
+		for(int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				grid[i][j] = newgrid[i][j];
+			}
+		}
+
+
 	}
 	
 	/**
@@ -24,6 +47,6 @@ public class TetrisGrid {
 	 * @return 2d grid array
 	 */
 	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
+		return grid; // YOUR CODE HERE
 	}
 }
